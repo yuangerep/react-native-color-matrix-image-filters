0c10541c4b9390fde087ec5ae977964aba14e241
wupingyuan <wupingyuan2@h-partners.com>
2024-04-23 16:20:54 +0800

    添加rn-color-matrices

--- rn-color-matrices/index.ts
+++ rn-color-matrices/index.ts
@@ -6,8 +6,8 @@ export type Matrix = [
   number, number, number, number, number, number, number, number, number, number
 ]
 
-const bias = Platform.OS === 'ios' ? 1 : 255
-const biasRev = Platform.OS === 'ios' ? 255 : 1
+const bias = 1;
+const biasRev = 255;
 
 const colorToRGB = (color: number) => [
   ((color >> 16) & 0xFF) / 255,
